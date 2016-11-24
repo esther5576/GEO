1626732d35c8dadb078b96b6073a7d80f18d1270
esther5576 <esther.berges@hotmail.com>
2016-11-24 20:23:42 +0100

    MenuPrincipal

--- CODE/Assets/C#/MenuManager.cs
+++ CODE/Assets/C#/MenuManager.cs
@@ -17,6 +17,7 @@ public class MenuManager : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		#region Start Panel Region
 		if (_rotateLogo) 
 		{
 			MenuVariables.panelStartAppLoupeElements.transform.localEulerAngles += new Vector3 (0, 0, 15) * Time.deltaTime;
@@ -25,26 +26,37 @@ public class MenuManager : MonoBehaviour
 			{
 				Debug.Log ("Close App");
 
-				LaunchMenuPrincipal ();
+				CloseStartMenu ();
 			}
 		}
+		#endregion
 	}
 
-	void StartApp(){
+	void StartApp()
+	{
 		MenuVariables.panelStartAppAppName.alpha = 0;
 		MenuVariables.panelStartAppLoupeElements.alpha = 0;
 		MenuVariables.panelStartAppLoupeElements.transform.localScale = Vector3.zero;
-
-		MenuVariables.panelStartAppTopCorner.transform.localPosition = new Vector3 (0, 40, 0);
 		MenuVariables.panelStartAppLoupe.transform.localPosition = new Vector3 (410, -610, 0);
 		MenuVariables.panelStartAppLoupeManche.transform.localPosition = new Vector3 (410, -610, 0);
+
+
+		MenuVariables.panelGeneralTopCorner.transform.localPosition = new Vector3 (0, 40, 0);
+
+
+		MenuVariables.panelPrincipalAppMissionsEnCours.transform.position = new Vector3 (460, -9, 0);
+		MenuVariables.panelPrincipalAppNouvellesMissions.transform.position = new Vector3 (460, -9, 0);
+		MenuVariables.panelPrincipalAppTakeJobButton.transform.position = new Vector3 (460, -9, 0);
+		MenuVariables.panelPrincipalAppMissionsEnCoursButton.transform.position = new Vector3 (460, 186, 0);
+		MenuVariables.panelPrincipalAppNouvellesMissionsButton.transform.position = new Vector3 (460, 186, 0);
+
 
 		StartTweens ();
 	}
 
 	void StartTweens ()
 	{
-		MenuVariables.panelStartAppTopCorner.transform.DOLocalMove (new Vector3 (0, -10, 0), 0.75f, false).SetEase (Ease.OutExpo);
+		MenuVariables.panelGeneralTopCorner.transform.DOLocalMove (new Vector3 (0, -10, 0), 0.75f, false).SetEase (Ease.OutExpo);
 
 		MenuVariables.panelStartAppLoupe.transform.DOLocalMove (new Vector3 (0, 0, 0), 0.3f, false).SetEase (Ease.OutExpo).SetDelay(0.55f);
 		MenuVariables.panelStartAppLoupeManche.transform.DOLocalMove (new Vector3 (0, 0, 0), 0.3f, false).SetEase (Ease.OutExpo).SetDelay(0.55f);
@@ -63,7 +75,7 @@ public class MenuManager : MonoBehaviour
 		_rotateLogo = !_rotateLogo;
 	}
 
-	void LaunchMenuPrincipal()
+	void CloseStartMenu()
 	{
 		ChangeBoolRotateLogo ();
 
@@ -77,5 +89,16 @@ public class MenuManager : MonoBehaviour
 		MenuVariables.panelStartAppLoupe.transform.DOLocalMove (new Vector3 (0, -900, 0), 0.6f, false).SetEase (Ease.InExpo).SetDelay(1.5f);
 
 		MenuVariables.panelStartAppMap.transform.DOLocalMove (new Vector3 (-500, 0, 0), 0.6f, false).SetEase (Ease.OutExpo).SetDelay(2f);
+
+		StartMenuPrincipal ();
+	}
+
+	void StartMenuPrincipal()
+	{
+		MenuVariables.panelPrincipalAppMissionsEnCours.transform.DOLocalMove (new Vector3 (0, -9, 0), 0.6f, false).SetEase (Ease.OutExpo).SetDelay(1.85f);
+		MenuVariables.panelPrincipalAppNouvellesMissions.transform.DOLocalMove (new Vector3 (0, -9, 0), 0.6f, false).SetEase (Ease.OutExpo).SetDelay(1.85f);
+		MenuVariables.panelPrincipalAppTakeJobButton.transform.DOLocalMove (new Vector3 (0, -9, 0), 0.6f, false).SetEase (Ease.OutExpo).SetDelay(1.85f);
+		MenuVariables.panelPrincipalAppMissionsEnCoursButton.transform.DOLocalMove (new Vector3 (0, 186, 0), 0.6f, false).SetEase (Ease.OutExpo).SetDelay(1.85f);
+		MenuVariables.panelPrincipalAppNouvellesMissionsButton.transform.DOLocalMove (new Vector3 (0, 186, 0), 0.6f, false).SetEase (Ease.OutExpo).SetDelay(1.85f);
 	}
 }
--- CODE/Assets/C#/MenuVariables.cs
+++ CODE/Assets/C#/MenuVariables.cs
@@ -3,22 +3,52 @@ using System.Collections;
 
 public class MenuVariables 
 {
+	#region Panel Start App
 	public static UIPanel panelStartApp; 
-	public static UISprite panelStartAppMap; 
-	public static UISprite panelStartAppTopCorner; 
+	public static UISprite panelStartAppMap;
 	public static UISprite panelStartAppAppName; 
 	public static UISprite panelStartAppLoupe; 
 	public static UISprite panelStartAppLoupeManche; 
 	public static UISprite panelStartAppLoupeElements;
+	#endregion
+
+	#region Panel General App
+	public static UIPanel panelGeneral; 
+	public static UISprite panelGeneralTopCorner; 
+	#endregion
+
+	#region Panel Principal App
+	public static UIPanel panelPrincipalApp; 
+	public static UISprite panelPrincipalAppNouvellesMissions; 
+	public static UISprite panelPrincipalAppMissionsEnCours; 
+	public static UIButton panelPrincipalAppTakeJobButton; 
+	public static UIButton panelPrincipalAppMissionsEnCoursButton; 
+	public static UIButton panelPrincipalAppNouvellesMissionsButton; 
+	#endregion
 
 	public static void InitVariables()
 	{
+		#region Panel Start App
 		panelStartApp = GameObject.Find ("UI Root/PanelStartApp").GetComponent<UIPanel>();
 		panelStartAppMap = GameObject.Find ("UI Root/PanelStartApp/Map").GetComponent<UISprite>();
-		panelStartAppTopCorner = GameObject.Find ("UI Root/GeneralPanel/Top Corner").GetComponent<UISprite>();
 		panelStartAppAppName = GameObject.Find ("UI Root/PanelStartApp/AppName").GetComponent<UISprite>();
 		panelStartAppLoupe = GameObject.Find ("UI Root/PanelStartApp/Loupe").GetComponent<UISprite>();
 		panelStartAppLoupeManche = GameObject.Find ("UI Root/PanelStartApp/LoupeManche").GetComponent<UISprite>();
 		panelStartAppLoupeElements = GameObject.Find ("UI Root/PanelStartApp/LoupeElements").GetComponent<UISprite>();
+		#endregion
+
+		#region Panel General App
+		panelGeneral = GameObject.Find ("UI Root/GeneralPanel").GetComponent<UIPanel>();
+		panelGeneralTopCorner = GameObject.Find ("UI Root/GeneralPanel/TopCorner").GetComponent<UISprite>();
+		#endregion
+
+		#region Panel Principal App
+		panelPrincipalApp = GameObject.Find ("UI Root/PanelPrincipalApp").GetComponent<UIPanel>();
+		panelPrincipalAppNouvellesMissions = GameObject.Find ("UI Root/PanelPrincipalApp/NouvellesMissions").GetComponent<UISprite>();
+		panelPrincipalAppMissionsEnCours = GameObject.Find ("UI Root/PanelPrincipalApp/MissionsEnCours").GetComponent<UISprite>();
+		panelPrincipalAppTakeJobButton = GameObject.Find ("UI Root/PanelPrincipalApp/TakeJobButton").GetComponent<UIButton>();
+		panelPrincipalAppMissionsEnCoursButton = GameObject.Find ("UI Root/PanelPrincipalApp/MissionsEnCoursButton").GetComponent<UIButton>(); 
+		panelPrincipalAppNouvellesMissionsButton = GameObject.Find ("UI Root/PanelPrincipalApp/NouvellesMissionsButton").GetComponent<UIButton>(); 
+		#endregion
 	}
 }
