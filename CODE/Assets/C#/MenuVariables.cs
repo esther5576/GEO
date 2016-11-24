a47e161e4dc32a2a66c2e1d41fb944c91fcb38f5
esther5576 <esther.berges@hotmail.com>
2016-11-24 12:42:30 +0100

    Graphs

---
+++ CODE/Assets/C#/MenuVariables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuVariables 
+{
+	public static GameObject panelStartApp; 
+	public static GameObject panelStartAppMap; 
+	public static GameObject panelStartAppTopCorner; 
+	public static GameObject panelStartAppAppName; 
+	public static GameObject panelStartAppLoupe; 
+	public static GameObject panelStartAppLoupeManche; 
+	public static GameObject panelStartApp
+
+	public static void InitVariables()
+	{
+		
+	}
+}
